7e80a2c6b48120ac4ed4b294f25d57b60af16cd4
James Lay <james@eventrentalsystems.com>
2026-06-06 12:05:27 -0600

    Initial commit from Create Wix App

---
+++ src/extensions/dashboard/pages/my-page/my-page.extension.ts
@@ -0,0 +1,8 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: '928792b5-2d2b-4c26-94b5-764a1aec0d85',
+  title: 'My Page',
+  routePath: '',
+  component: './extensions/dashboard/pages/my-page/my-page.tsx',
+});
