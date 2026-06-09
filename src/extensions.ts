aab7e05c6606c85fc9e3973c10ce02b2c94e3cec
james lay <james@mod3.tech>
2026-06-08 21:30:45 -0600

    Switch to plain collection ID, remove data collection extension

--- src/extensions.ts
+++ src/extensions.ts
@@ -1,9 +1,7 @@
 import { app } from '@wix/astro/builders';
 import myPage from './extensions/dashboard/pages/my-page/my-page.extension.ts';
 
-import dataCollections from './extensions/backend/data-collections/data-collections.extension.ts';
-
 import memberProfileForm from './extensions/site/widgets/member-profile-form/member-profile-form.extension.ts';
 
 export default app()
-  .use(myPage).use(dataCollections).use(memberProfileForm);
+  .use(myPage).use(memberProfileForm);
