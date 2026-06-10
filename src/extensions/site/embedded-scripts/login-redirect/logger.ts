2261f3f813869bf270dd666dc519cf133e15abd6
james lay <james@mod3.tech>
2026-06-10 17:40:04 -0600

    Add visible red banner to test embedded script injection

--- src/extensions/site/embedded-scripts/login-redirect/logger.ts
+++ src/extensions/site/embedded-scripts/login-redirect/logger.ts
@@ -1,5 +1,1 @@
-console.log('[Login Redirect] Script loaded, logging in 3 seconds...');
-setTimeout(() => {
-  console.log('[Login Redirect] 3 seconds have passed');
-  document.title = '[LOGIN REDIRECT ACTIVE] ' + document.title;
-}, 3000);
+console.log('[Login Redirect] Script loaded, hi!');
